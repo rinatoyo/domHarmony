9924c739e2babf441cfc3a7fbc0d1d1baedef812
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-01-23 21:38:30 -1000

    #6

--- js/app.js
+++ js/app.js
@@ -29,6 +29,11 @@ data.appendChild(newDiv);
 
 //6. In the div id data section, create a div with an id of hobbies and give it the following information: Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.
 
+var divHobbies = document.createElement('div');
+divHobbies.id = 'hobbies';
+divHobbies.innerHTML = 'Long romantic walks on the beach, candle light Big Mac dinners and tormenting the King.';
+data.appendChild(divHobbies);
+
 //7. In the div id data section, create a div with an id of location and give it the following information: Honolulu, HI.
 
 //8. In the div id data section, create a div with an id of wants and give it the following information: Looking for a Mrs. McDonald.
